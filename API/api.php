bfb6af6d954efabf02f0b85bbad8d2a193576673
Fernando Walpert <walllpapert@gmail.com>
2025-08-29 08:16:05 -0300

    cambios
    - actualizadas funciones de usuarios en API

--- API/api.php
+++ API/api.php
@@ -36,39 +36,16 @@ switch ($method) {
 			// Añade un nuevo usuario
 			$data = json_decode(file_get_contents('php://input'), true);
 			$result = $usuarioObj->addUsuario($data);
-			if ($result === true) {
-				http_response_code(201);
-				echo json_encode(['success' => $result]);
-			}else{
-				http_response_code(400);
-				echo json_encode(['error' => 'Datos incompletos o error al registrar usuario']);
-			}
+			echo $result;
 		}elseif ($endpoint === '/login') {
 			$data = json_decode(file_get_contents('php://input'), true);
 			$result = $usuarioObj->loginUsuario($data['usr_email'], $data['usr_pass']);
-			if ($result != false) {
-				echo json_encode(['success' => true, 'usuario' => $result]);
-			} else {
-				http_response_code(401);
-				echo json_encode(['error' => 'Credenciales incorrectas']);
-			}
+			echo $result;
 		}elseif ($endpoint === '/logout') {
 			// Cierra sesión del usuario
 			$data = json_decode(file_get_contents('php://input'), true);
-			if (isset($data['usr_key'])) {
-				// Aquí podrías implementar la lógica para cerrar sesión, como invalidar la clave de sesión
-				$result = $usuarioObj->logoutUsuario( $data['usr_key']);
-				if($result === true) {
-					http_response_code(200);
-					echo json_encode(['success' => true, 'message' => 'Sesión cerrada']);
-				} else {
-					http_response_code(400);
-					echo json_encode(['error' => 'Error al cerrar sesión']);
-				}
-			} else {
-				http_response_code(400);
-				echo json_encode(['error' => 'Datos incompletos']);
-			}
+			$result = $usuarioObj->logoutUsuario( $data);
+			echo $result;
 		}
 		break;
 	case 'PUT':
